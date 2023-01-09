46a139ac33e9bfc2c222bec53d8949e10d384755
Smartoneinok <Alexander_Novotny@aol.com>
2023-01-09 16:29:00 -0600

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # box-open-ai-skill-demo
 This repository contains an example GCP function that accepts a Box Skill invocation and calls the OpenAI TLDR service.
+
+**Note: You will need to add a gcp crendentials file and update the serverless.yml with that appropriate connection information.**
+
+You will need to set up a Box Skill in the Box Developer Console, as well as authorize the skill and configure a folder to watch for uploads. You can find out more about that process in our [developer documentation](https://developer.box.com/guides/applications/custom-skills/setup/). 
+
+## Steps to Setup and Deploy
+
+1. Install Node v10.0.0 or higher
+2. [Set up a Google Cloud Account](https://serverless.com/framework/docs/providers/google/guide/credentials/)
+3. Download the code.
+4. Add your google keyfile to the .gcloud folder and name is serverless.json.
+5. Update any connection and configuration information in the serverless.yml and package.json files. 
+6. `npm install`
+7. `sls deploy`
+8. Once the deploy is complete, copy the invocation URL and paste it into the Box Skill configuration section. Click Save.
+9. After deploying, in the GCP console, you'll need to allow public access to the function so Box can call it. Find your function in the GCP cloud functions dashboard. Under the permissions tab, grant access to `allUsers` with the `Cloud Functions Invoker` role. 
+
+Now, if you upload a text based file to the folder configured for the Box Skill, you should see a summary attached, as well as logs under the logs tab in the function.
---
+++ index.js
@@ -0,0 +1,127 @@
+'use strict';
+
+const { FilesReader, SkillsWriter, SkillsErrorEnum } = require('./skills-kit-2.0');
+const Box = require("box-node-sdk");
+const Axios = require("axios");
+const { Configuration, OpenAIApi } = require("openai");
+const {encode, decode} = require('gpt-3-encoder')
+
+//DESCRIPTION: This Box Skill serverless function takes text from a file stored in Box and sends it to OpenAI's completion API endpoint to create a Skill card tl;dr summary. 
+//In addition to this code, which is written to be deployed to a GCP function, you will need to set up a Box Skill in the Box developer console, authorize and configure the Box Skill, and set up an account with OpenAI.
+//Visit the readme to find out more on the steps to deploy
+//You can chose the open ai model selected on https://beta.openai.com/docs/models/gpt-3. This example uses davinci which maxes out at 4096 tokens. 
+
+//environment variables
+const boxPrimaryKey = process.env.box_primary_key;
+const boxSecondaryKey = process.env.box_secondary_key;
+const box_api_endpoint = process.env.box_api_endpoint;
+const open_ai_model = process.env.open_ai_model;
+const max_document_tokens = process.env.max_document_tokens;
+
+exports.boxSkill = async (request, response) => {      
+    const filesReader = new FilesReader(request.body);
+    const skillsWriter = new SkillsWriter(filesReader.getFileContext());
+    var text;
+    var encoded;
+
+        try{
+            const body = JSON.stringify(request.body);
+            console.log('Request Body: ', body);
+            //Validate Bpx Signature Keys So Bad People Don't Use Your Endpoint
+            let isValid = Box.validateWebhookMessage(request.body, request.headers, boxPrimaryKey, boxSecondaryKey);  
+            if(isValid){
+                //You can use this code to save a temporary processing skills card if using the skills card option
+                await skillsWriter.saveProcessingCard();
+                //function to download the text
+                await downloadText();
+                //function to count tokens
+                await countTokens();
+                //function to find the summary of the document and save it back to skill card
+                await findSummary();
+                console.log("Skill process completed.")
+                // Skills engine requires a 200 response within 10 seconds of sending an event.
+                response.status(200).send('Box event was processed by skill');
+            } else {
+                console.log('Keys Were Not Valid')
+                response.status(401).send('Unauthorized');
+            }
+
+        } catch (error) {
+            await skillsWriter.saveErrorCard(SkillsErrorEnum.UNKNOWN,"The text extract was not ready. It will retry again in a few minutes.");
+            console.error(
+                `Skill processing failed for file: ${filesReader.getFileContext().fileId} with error: ${error.message}`
+            );
+            response.status(400).send('Something went wrong. The process will retry via exponential backoff.');
+        }
+
+        async function downloadText() {
+            //Build the file download url so that we can locally download the extracted text
+            var textDownloadURL = `${box_api_endpoint}/${request.body.source.id}/versions/${request.body.source.file_version.id}/representations/extracted_text/content/?access_token=${request.body.token.read.access_token}`;
+            console.log("download url: ", textDownloadURL);
+            //Wait approx 5 seconds for Box to finish the text extract... maybe
+            await new Promise(resolve => setTimeout(resolve, 5000));
+            //Download text from Box
+            var { data } = await Axios.get(textDownloadURL);
+            //assign data to text variable
+            text = data;
+            //Check the string for how many tokens it will take
+            encoded = encode(data);
+            //Check to make sure there was text. If there is not, the text needs to be redownloaded.
+            //Will send error back to back causing exponintial backoff retry.
+            if (encoded.length == 0){
+                throw new Error("Text extract not ready.");
+            } else {
+                console.log("Text Extracted Correctly.");
+            }
+        }
+
+        async function countTokens () {
+            console.log('This text contains this many tokens: ', encoded.length);
+            //If the text exceeds the openai model token max, you need to reduce the text
+            console.log('Max tokens: ', max_document_tokens);
+
+            if (encoded.length >= max_document_tokens) {
+                //start counter
+                let i = 0;
+                //reset text
+                let textEdited = "";
+                //loop through tokens and recombine text within appropriate limit
+                for(let token of encoded){
+                    if(i >= max_document_tokens){break};
+                    textEdited = textEdited + decode([token]);
+                    i++;
+                }
+                text = textEdited;
+            }
+        }
+
+        async function findSummary() {
+            //Create OpenAI configuration as defined in their developer documentation
+            const configuration = new Configuration({
+                apiKey: process.env.openai_sec_key,
+                });
+            const openai = new OpenAIApi(configuration);
+            //Call OpenAI with text extracted from document
+            const responseOpenAI = await openai.createCompletion({
+                model: open_ai_model,
+                prompt: text + "\n\ntl;dr",
+                temperature: 0.7,
+                max_tokens: 275,
+                top_p: 1,
+                frequency_penalty: 0,
+                presence_penalty: 1,
+            });
+            if (responseOpenAI.data.choices[0].text){
+                console.log("OpenAI Text: ", JSON.stringify(responseOpenAI.data.choices[0].text));
+                //Create a transcript card with the tldr summary from OpenAI
+                const cards = [];
+                const mockListOfDiscoveredTranscripts = [{ text: responseOpenAI.data.choices[0].text }];
+                cards.push(skillsWriter.createTranscriptsCard(mockListOfDiscoveredTranscripts));
+                console.log(`cards ${JSON.stringify(cards)}`);
+                //Write the card onto the file
+                await skillsWriter.saveDataCards(cards);
+            } else {
+                await skillsWriter.saveErrorCard(SkillsErrorEnum.UNKNOWN);
+            }
+        }
+};
